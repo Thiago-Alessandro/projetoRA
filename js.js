16ad923cf6ef4e962a89196f7fbc3e1906c72bc3
Thiago Alessandro Batista <alebatistathiago@gmail.com>
2023-09-11 19:43:07 -0300

    teste

--- js.js
+++ js.js
@@ -1,6 +1,8 @@
 let target = document.querySelector("#target");
+let divTarget = document.querySelector("#divTarget")
 let comecar = true;
 
 if(comecar){
     console.log(target);
+    console.log(divTarget)
 }
