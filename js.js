82a9efb4b4d7b6435f34da3bb4475cb1802c5890
Thiago Alessandro Batista <alebatistathiago@gmail.com>
2023-09-11 20:18:22 -0300

    teste

--- js.js
+++ js.js
@@ -4,19 +4,24 @@ let comecou = false;
 
 let botao = document.querySelector("#botao")
 
+divTarget.outerHTML = "";
 divTarget.innerHTML = "";
-divTarget.appendChild(botao)
-if(comecou){
+divTarget.outerHTML = botao.outerHTML
+divTarget.innerHTML = botao.innerHTML
+
+if (comecou) {
     // console.log(divTarget.innerHTML)
     // //divTarget.innerHTML = ""
     // console.log(divTarget)
     // console.log(target)
-    
+
 }
 
 
-function comecar(){
-    comecou = true;
-    divTarget.innerHTML = ""
-    divTarget.appendChild(target);
+function comecar() {
+    divTarget.outerHTML = "";
+    divTarget.innerHTML = "";
+    divTarget.outerHTML = target.outerHTML
+    divTarget.innerHTML = target.innerHTML
+
 }
